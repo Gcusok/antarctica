06ad3f084fea6a8d7f91133c1f1f5e8edb449688
Олег <matrix425@yandex.ru>
2023-04-04 12:45:18 +0300

    вносит правки в раздел карточек товаров

--- source/js/modules/slow-scroll/slow-scroll.js
+++ source/js/modules/slow-scroll/slow-scroll.js
@@ -2,6 +2,7 @@ const navigationButtons = document.querySelectorAll('[data-nav-button]');
 const advantagesSection = document.querySelector('[data-section="advantages"]');
 const bookingSection = document.querySelector('[data-section="booking"]');
 const contactsSection = document.querySelector('[data-section="contacts"]');
+const modal = document.querySelector('[data-modal="feedback"]');
 
 const initSlowScroll = () => {
 
@@ -9,6 +10,9 @@ const initSlowScroll = () => {
         button.addEventListener('click', (evt) => {
             evt.preventDefault();
             let section;
+            if (modal.classList.contains('is-active')) {
+                modal.classList.remove('is-active');
+            }
             switch (evt.target.textContent) {
                 case 'Преимущества':
                     section = advantagesSection;
